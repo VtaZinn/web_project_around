8987a0e4003da557bb1c76a58aa60edd34561178
Victor <augustov416@gmail.com>
2024-03-26 16:08:34 -0300

    Implementação do projeto

---
+++ script/index.js
@@ -0,0 +1,31 @@
+let buttonEdit = document.getElementById("ButtonEdit");
+let buttonClose = document.getElementById("buttonClose");
+let buttonSave = document.getElementById("buttonSave");
+buttonEdit.addEventListener("click", openModal);
+buttonClose.addEventListener("click", closeModal);
+buttonSave.addEventListener("click", save);
+
+let nameUser = document.getElementById("name");
+let aboutMe = document.getElementById("aboutMe");
+let inputName = document.getElementById("inputName");
+let inputAboutMe = document.getElementById("inputAboutMe");
+
+let modal = document.getElementById("modal");
+
+function openModal() {
+  inputName.value = nameUser.innerText;
+  inputAboutMe.value = aboutMe.innerText;
+  modal.classList.add("popup_opened");
+}
+
+function closeModal() {
+  modal.classList.remove("popup_opened");
+}
+
+function save() {
+  nameUser.innerText = inputName.value;
+  aboutMe.innerText = inputAboutMe.value;
+  closeModal();
+}
+
+
